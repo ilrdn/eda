cda9b9d511d7896c8aadcd853f237e098ab36bc8
ilrdn <49016594+ilrdn@users.noreply.github.com>
2020-12-24 22:57:46 +0400

    code style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
 function hide(element) {
+    /* К сожалению обычной css анимацией невозможно поставить "display: none;", 
+    поэтому выполняем данный скрипт */
     if (!element.classList.contains('fadeOut')) {
         element.classList.add('fadeOut');
         setTimeout(function() {
@@ -10,25 +12,6 @@ function hide(element) {
         element.classList.remove('fadeOut');
         element.classList.add('fadeIn');
     }
-    /* element.classList.add("fadeOut"); */
-    /* К сожалению обычной css анимацией невозможно поставить "display: none;", 
-    поэтому выполняем данный скрипт */
-   /*  if (element.classList.contains('hidden')) {
-        element.classList.remove('hidden');
-        element.classList.remove('visuallyhidden');
-        setTimeout(function () {
-            element.classList.remove('visuallyhidden');
-        }, 20);
-    } else {
-        element.classList.add('visuallyhidden');
-        element.addEventListener('transitionend', function(e) {
-            element.classList.add('hidden');
-        }, {
-            capture: false,
-            once: true,
-            passive: false
-        });
-    } */
 }
 
 let btn = document.getElementById('locBtn');

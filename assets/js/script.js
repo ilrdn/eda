bffe092b5b5f06bf53ed57ffd43c07f24f8edb30
ilrdn <49016594+ilrdn@users.noreply.github.com>
2021-01-11 14:59:29 +0400

    part 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,21 @@
-function hide(element) {
+/* не использовал стрелочные функции, чтобы IE не грустил */
+
+var phoneMask = IMask(
+    document.getElementById('phone-mask'), {
+        mask: '+{7}(000)000-00-00'
+    }
+);
+
+var cards = tns({
+    container: '.cards',
+    loop: false,
+    items: 3,
+    gutter: 20,
+    controlsText: ['', ''],
+    mouseDrag: true
+});
+
+function visibility(element) {
     /* К сожалению обычной css анимацией невозможно поставить "display: none;", 
     поэтому выполняем данный скрипт */
     if (!element.classList.contains('fadeOut')) {
@@ -14,23 +31,68 @@ function hide(element) {
     }
 }
 
-let btn = document.getElementById('locBtn');
-let closeBtn = document.getElementById('locClose');
-let input = document.getElementById('locInput');
-let list = document.getElementById('locList');
+var locBtn = document.getElementById('locBtn');
+var locClose = document.getElementById('locClose');
+var input = document.getElementById('locInput');
+var list = document.getElementById('locList');
+var locWrap = document.getElementById('locWrap');
 
-btn.onclick = function() {
-    hide(this);
+var srBtn = document.getElementById('srBtn');
+var srClose = document.getElementById('srClose');
+var srWrap = document.getElementById('srWrap');
+
+var loginBtn = document.getElementById('loginBtn');
+var modal = document.getElementById('modal');
+var modalClose = document.getElementById('modalClose');
+
+locBtn.onclick = function() {
+    //Скрываем другой input, так как оба не умещаются
+    if(srBtn.classList.contains('hidden')) {
+        visibility(srWrap);
+        visibility(srBtn);
+    };
+    visibility(this);
+    visibility(locWrap);
 };
 
-closeBtn.onclick = function() {
-    hide(btn);
+locClose.onclick = function() {
+    visibility(locBtn);
+    visibility(locWrap);
 };
 
 input.onfocus = function() {
-    hide(list);
+    visibility(list);
 };
 
 input.onblur = function() {
-    hide(list);
+    visibility(list);
+};
+
+srBtn.onclick = function() {
+    //Скрываем другой input, так как оба не умещаются
+    if(locBtn.classList.contains('hidden')) {
+        visibility(locWrap);
+        visibility(locBtn);
+    };
+    visibility(this);
+    visibility(srWrap);
+};
+
+srClose.onclick = function() {
+    visibility(srBtn);
+    visibility(srWrap);
+};
+
+loginBtn.onclick = function() {
+    visibility(modal);
+};
+
+modalClose.onclick = function() {
+    visibility(modal);
+};
+
+modal.onclick = function(e) {
+    if (e.target === this) {
+        visibility(this);
+    }
 };
